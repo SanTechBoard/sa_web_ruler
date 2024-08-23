16b20220a45dc90637a718cbb5d4de8c1f1c2470
SanTechBoard <141046951+SanTechBoard@users.noreply.github.com>
2024-08-23 15:58:43 +0530

    Update slave_rec.ino

--- ultrasonic ruler arduino/slave_rec/slave_rec.ino
+++ ultrasonic ruler arduino/slave_rec/slave_rec.ino
@@ -1,7 +1,7 @@
-
 #include <esp_now.h>
 #include <WiFi.h>
 
+// Define a structure to hold the incoming data
 typedef struct struct_message {
   char a[32];
   int b;
@@ -9,43 +9,45 @@ typedef struct struct_message {
   bool d;
 } struct_message;
 
+// Create an instance of the structure to hold the received data
 struct_message myData;
 
+// Create a peer info structure
 esp_now_peer_info_t peerInfo;
 
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len) {
+// Callback function that will be called when data is received
+void OnDataRecv(const esp_now_recv_info_t *info, const uint8_t *incomingData, int len) {
   memcpy(&myData, incomingData, sizeof(myData));
-  // Serial.print("Data received: ");
-  // Serial.println(len);
-  // Serial.print("Character Value: ");
+
+  // Print the received data
   Serial.println(myData.a);
-  // Serial.print("Integer Value: ");
-  // Serial.println(myData.b);
-  // Serial.print("Float Value: ");
-  // Serial.println(myData.c);
-  // Serial.print("Bool Value: ");
-  // Serial.println(myData.d);
-  // Serial.println();
 }
 
 void setup() {
+  // Initialize serial communication
   Serial.begin(115200);
 
+  // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
+  // Initialize ESP-NOW
   if (esp_now_init() != ESP_OK) {
     Serial.println("Error initializing ESP-Now");
     return;
   }
 
-  esp_now_register_recv_cb((esp_now_recv_cb_t)OnDataRecv);
+  // Register the receive callback function
+  esp_now_register_recv_cb(OnDataRecv);
 
   // Add the MAC address of the sender
   uint8_t senderMacAddress[] = {mac_id};
+  memset(&peerInfo, 0, sizeof(peerInfo)); // Ensure no garbage values
   memcpy(peerInfo.peer_addr, senderMacAddress, 6);
   peerInfo.channel = 0;
   peerInfo.encrypt = false;
 
+  
+  // Add the peer
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
     Serial.println("Failed to add peer");
     return;
@@ -53,4 +55,6 @@ void setup() {
 }
 
 void loop() {
+  // Nothing to do here, everything is handled by the callback function
 }
+
